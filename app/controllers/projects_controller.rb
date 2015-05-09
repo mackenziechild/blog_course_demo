4cc4eaa695b2424e74e0cd8cbe43607d1b4eed27
Mackenzie Child <mackenzie.child@gmail.com>
2015-05-09 11:38:51 -0600

    Add edit & destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,22 @@ class ProjectsController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Nice Mackenzie! That project was successfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def find_project
