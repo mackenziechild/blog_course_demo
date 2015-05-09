05f3d3b17a485f1a2d6109757d71b1969d58994b
Mackenzie Child <mackenzie.child@gmail.com>
2015-05-09 10:12:31 -0600

    Added Redcarpet & Pygments.rb for Markdown & Syntax Highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+	def markdown(content)
+		renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+		options = {
+			autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+		}
+		Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+	end
 end
