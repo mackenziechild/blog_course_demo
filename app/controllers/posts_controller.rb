0d04c1e8f518df19559a4e7c51816848a747944f
Mackenzie Child <mackenzie.child@gmail.com>
2015-05-08 21:35:22 -0600

    Added Post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Hellz yeah, Mackenzie! Your article was successfully saved!"
+		else
+			render 'new', notice: "Oh no, Mackenzie! I was unable to save your post."
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Huzzah! Your article was successfully saved!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+end
